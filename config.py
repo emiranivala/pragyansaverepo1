a8509db0306218fb9d41028a9f413e57bf375098
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2025-02-13 07:36:17 +0000

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,21 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+API_ID = int(getenv("API_ID", "20348897"))
+API_HASH = getenv("API_HASH", "ecfb9a700b8398caf58c53f36ccf5b06")
+BOT_TOKEN = getenv("BOT_TOKEN","8037339627:AAE1aJmJXcerLti5xuH-aDUGxFJ5Yd51KAw")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5464921200").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://PRAGYAN:PRAGYAN@cluster0.w3eiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002283846449")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001573605549"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "500"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
 STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
-INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
+
+# YouTube Cookies Configuration (ensure to replace this with valid cookies)
+#YT_COOKIES = getenv("YT_COOKIES", "GPS=1; YSC=VW83QNuF-rE; __Secure-ROLLOUT_TOKEN=CPfXrsuG1-m97gEQ-L_JhfWBiwMY-L_JhfWBiwM%3D; VISITOR_INFO1_LIVE=myhtpsWPqDg; VISITOR_PRIVACY_METADATA=CgJJThIEGgAgUg%3D%3D; PREF>
+
+# Instagram Cookies (if required)
+#INSTA_COOKIES = getenv("INSTA_COOKIES", None)
